0cc2320b6b22ea91a3c1e4865c967d44dca4c48f
Elpsys <1437432373@qq.com>
2019-06-14 21:58:13 +0800

    shouye

---
+++ ChineseChess/src/checkerboard/Checkerboard.java
@@ -0,0 +1,54 @@
+package checkerboard;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+
+public class Checkerboard {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Checkerboard window = new Checkerboard();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Checkerboard() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 650, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		ImageIcon image=new ImageIcon("D:\\JAVAPROJECT\\WORKSPACE\\ChineseChess\\images\\homepage.jpg");
+		JLabel label=new JLabel(image); frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(label); label.setBounds(0,0,650,800);
+		frame.setVisible(true); 
+		frame.setResizable(false);
+
+	}
+}
