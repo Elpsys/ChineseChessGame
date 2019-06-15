67cfff081b21a6453d3fe0e8cb1c790b2c4c2a1a
Elpsys <1437432373@qq.com>
2019-06-15 22:02:33 +0800

    haha

--- ChineseChess/src/checkerboard/Checkerboard.java
+++ ChineseChess/src/checkerboard/Checkerboard.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import java.awt.Image;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -44,11 +45,20 @@ public class Checkerboard {
 		frame.setBounds(100, 100, 650, 800);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		ImageIcon image=new ImageIcon("images/homepage.jpg");
-		JLabel label=new JLabel(image); frame.getContentPane().setLayout(null); 
-		frame.getContentPane().add(label); label.setBounds(0,0,650,800);
+		ImageIcon imagehome=new ImageIcon("images/homepage.jpg");
+		JLabel label=new JLabel(imagehome); 
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(label);
+		label.setBounds(0,0,650,800);
 		frame.setVisible(true); 
 		frame.setResizable(false);
-
+		ImageIcon imagebutton=new ImageIcon("images/buttonstart.png");
+		JButton button=new JButton(imagebutton); 
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(button); 
+		button.setBounds(155,503,340,48);
+		frame.setVisible(true); 
+		frame.setResizable(false);
+		System.out.println("hahah");
 	}
 }
