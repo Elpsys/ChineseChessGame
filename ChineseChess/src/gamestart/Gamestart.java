676ab94dc74dda7a6b42f60517d22f934ab97492
Elpsys <1437432373@qq.com>
2019-06-18 11:54:36 +0800

    棋子

--- ChineseChess/src/gamestart/Gamestart.java
+++ ChineseChess/src/gamestart/Gamestart.java
@@ -1,15 +1,21 @@
 package gamestart;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Gamestart extends JFrame{
 	public static JFrame frame;
+	
 	public static void chessboard()
 	{
 		frame=new JFrame("ÖÐ¹úÏóÆå");
@@ -23,7 +29,23 @@ public class Gamestart extends JFrame{
 		cp.setLayout(new BorderLayout());		
 		((JPanel)cp).setOpaque(false);	
 		
+		//
 		
+		ImageIcon imagechesssoldier= new ImageIcon("images/redsoldier.png");
+		JButton soldierbutton1 = new JButton(imagechesssoldier);		
+		soldierbutton1.setBounds(71, 397, 52, 52);
+		soldierbutton1.setContentAreaFilled(false);
+		soldierbutton1.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(soldierbutton1);
+		soldierbutton1.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+			
+		});
 		
 		frame.setVisible(true); 
 		frame.setResizable(false);
@@ -31,4 +53,5 @@ public class Gamestart extends JFrame{
 	public static void main(String[] args) {
 		chessboard();
 	}
+
 }
