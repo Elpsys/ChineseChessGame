39cf4aac86e105b73a8dfb9b2c81cf568bbbc55a
Elpsys <1437432373@qq.com>
2019-06-23 00:16:12 +0800

    黑方棋子

--- ChineseChess/src/gamestart/Gamestart.java
+++ ChineseChess/src/gamestart/Gamestart.java
@@ -25,6 +25,7 @@ public class Gamestart extends JFrame{
 	public static JFrame frame;
 	static boolean rdClicked;
 	static JButton redsoldier1,redsoldier2,redsoldier3,redsoldier4,redsoldier5,Lredgun,Rredgun,Lredcar,Rredcar,Lredhorse,Rredhorse,Lredelephant,Rredelephant,Lredknight,Rredknight,redcommander;
+	static JButton blacksoldier1,blacksoldier2,blacksoldier3,blacksoldier4,blacksoldier5,Lblackgun,Rblackgun,Lblackcar,Rblackcar,Lblackhorse,Rblackhorse,Lblackelephant,Rblackelephant,Lblackknight,Rblackknight,blackcommander;
 	static int rd;//ÅÐ¶Ï±øµÄ²»Í¬
 	static int x;
 	static int y;
@@ -264,6 +265,231 @@ public class Gamestart extends JFrame{
 				rd = 5;
 			}
 		});			
+		
+		//------------------------------------------------------------------------------
+		
+		//ºÚ·½±ø1
+		ImageIcon imagebd;
+		imagebd = new ImageIcon("images/blacksoldier.png");
+		blacksoldier1 = new JButton(imagebd);	
+		blacksoldier1.setBounds(71, 245, 52, 52);
+		blacksoldier1.setContentAreaFilled(false);
+		blacksoldier1.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(blacksoldier1);
+		blacksoldier1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 1;
+			}
+		});
+		//ºÚ·½±ø2
+		blacksoldier2 = new JButton(imagebd);	
+		blacksoldier2.setBounds(184, 245, 52, 52);
+		blacksoldier2.setContentAreaFilled(false);
+		blacksoldier2.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(blacksoldier2);
+		blacksoldier2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 2;
+			}
+		});
+		//ºÚ·½±ø3
+		blacksoldier3 = new JButton(imagebd);	
+		blacksoldier3.setBounds(299, 245, 52, 52);
+		blacksoldier3.setContentAreaFilled(false);
+		blacksoldier3.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(blacksoldier3);
+		blacksoldier3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 3;
+			}
+		});
+		//ºÚ·½±ø4
+		blacksoldier4 = new JButton(imagebd);	
+		blacksoldier4.setBounds(415, 245, 52, 52);
+		blacksoldier4.setContentAreaFilled(false);
+		blacksoldier4.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(blacksoldier4);
+		blacksoldier4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 4;
+			}
+		});
+		//ºÚ·½±ø5
+		blacksoldier5 = new JButton(imagebd);	
+		blacksoldier5.setBounds(530, 245, 52, 52);
+		blacksoldier5.setContentAreaFilled(false);
+		blacksoldier5.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(blacksoldier5);
+		blacksoldier5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});
+		//ºÚ·½ÅÚ×ó
+		ImageIcon imagegun1;
+		imagegun1 = new ImageIcon("images/blackgun.png");
+		Lblackgun = new JButton(imagegun1);	
+		Lblackgun.setBounds(127, 193, 52, 52);
+		Lblackgun.setContentAreaFilled(false);
+		Lblackgun.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lblackgun);
+		Lblackgun.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºÚ·½ÅÚÓÒ
+		Rblackgun = new JButton(imagegun1);	
+		Rblackgun.setBounds(473, 193, 52, 52);
+		Rblackgun.setContentAreaFilled(false);
+		Rblackgun.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rblackgun);
+		Rblackgun.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºÚ³µ×ó
+		ImageIcon imagecar1;
+		imagecar1 = new ImageIcon("images/blackcar.png");
+		Lblackcar = new JButton(imagecar1);	
+		Lblackcar.setBounds(71, 96, 52, 52);
+		Lblackcar.setContentAreaFilled(false);
+		Lblackcar.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lblackcar);
+		Lblackcar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºÚ³µÓÒ
+		Rblackcar = new JButton(imagecar1);	
+		Rblackcar.setBounds(530, 96, 52, 52);
+		Rblackcar.setContentAreaFilled(false);
+		Rblackcar.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rblackcar);
+		Rblackcar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºÚÂí×ó
+		ImageIcon imagehorse1;
+		imagehorse1 = new ImageIcon("images/blackhorse.png");
+		Lblackhorse = new JButton(imagehorse1);	
+		Lblackhorse.setBounds(127, 96, 52, 52);
+		Lblackhorse.setContentAreaFilled(false);
+		Lblackhorse.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lblackhorse);
+		Lblackhorse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºÚÂíÓÒ
+		Rblackhorse = new JButton(imagehorse1);	
+		Rblackhorse.setBounds(473, 96, 52, 52);
+		Rblackhorse.setContentAreaFilled(false);
+		Rblackhorse.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rblackhorse);
+		Rblackhorse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºÚÏó×ó
+		ImageIcon imageelephant1;
+		imageelephant1 = new ImageIcon("images/blackelep.png");
+		Lblackelephant = new JButton(imageelephant1);	
+		Lblackelephant.setBounds(184, 96, 52, 52);
+		Lblackelephant.setContentAreaFilled(false);
+		Lblackelephant.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lblackelephant);
+		Lblackelephant.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºÚÏóÓÒ
+		Rblackelephant = new JButton(imageelephant1);	
+		Rblackelephant.setBounds(416, 96, 52, 52);
+		Rblackelephant.setContentAreaFilled(false);
+		Rblackelephant.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rblackelephant);
+		Rblackelephant.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºÚÊ¿×ó
+		ImageIcon imageknight1;
+		imageknight1 = new ImageIcon("images/blackknight.png");
+		Lblackknight = new JButton(imageknight1);	
+		Lblackknight.setBounds(241, 96, 52, 52);
+		Lblackknight.setContentAreaFilled(false);
+		Lblackknight.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lblackknight);
+		Lblackknight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºÚÊ¿ÓÒ
+		Rblackknight = new JButton(imageknight1);	
+		Rblackknight.setBounds(359, 96, 52, 52);
+		Rblackknight.setContentAreaFilled(false);
+		Rblackknight.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rblackknight);
+		Rblackknight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºÚË§
+		ImageIcon imagecommander1;
+		imagecommander1 = new ImageIcon("images/blackcommander.png");
+		blackcommander = new JButton(imagecommander1);	
+		blackcommander.setBounds(298, 96, 52, 52);
+		blackcommander.setContentAreaFilled(false);
+		blackcommander.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(blackcommander);
+		blackcommander.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
 		frame.addMouseListener(new MyFrameMouse());
 		frame.setVisible(true); 
 		frame.setResizable(false);		
