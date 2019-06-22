c6e662f859135c515314f9a5ca732cdbccaca174
Elpsys <1437432373@qq.com>
2019-06-22 18:09:55 +0800

    红方棋子

--- ChineseChess/src/gamestart/Gamestart.java
+++ ChineseChess/src/gamestart/Gamestart.java
@@ -23,9 +23,9 @@ import javax.swing.JPanel;
 
 public class Gamestart extends JFrame{
 	public static JFrame frame;
-	static boolean isClicked;
-	static ImageIcon imagechesssoldier;
-	static JButton soldierbutton1;
+	static boolean rdClicked;
+	static JButton redsoldier1,redsoldier2,redsoldier3,redsoldier4,redsoldier5,Lredgun,Rredgun,Lredcar,Rredcar,Lredhorse,Rredhorse,Lredelephant,Rredelephant,Lredknight,Rredknight,redcommander;
+	static int rd;//ÅÐ¶Ï±øµÄ²»Í¬
 	static int x;
 	static int y;
 	
@@ -42,23 +42,232 @@ public class Gamestart extends JFrame{
 		cp.setLayout(new BorderLayout());		
 		((JPanel)cp).setOpaque(false);	
 		
-		// 
-		imagechesssoldier = new ImageIcon("images/redsoldier.png");
-		soldierbutton1 = new JButton(imagechesssoldier);	
-		soldierbutton1.setBounds(71, 397, 52, 52);
-		soldierbutton1.setContentAreaFilled(false);
-		soldierbutton1.setBorderPainted(false);
+		//ºì·½±ø1
+		ImageIcon imagerd;
+		imagerd = new ImageIcon("images/redsoldier.png");
+		redsoldier1 = new JButton(imagerd);	
+		redsoldier1.setBounds(71, 399, 52, 52);
+		redsoldier1.setContentAreaFilled(false);
+		redsoldier1.setBorderPainted(false);
 		frame.getContentPane().setLayout(null); 
-		frame.getContentPane().add(soldierbutton1);
-		soldierbutton1.addActionListener(new ActionListener() {
+		frame.getContentPane().add(redsoldier1);
+		redsoldier1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				isClicked = true;
+				rdClicked = true;
+				rd = 1;
 			}
 		});
+		//ºì·½±ø2
+		redsoldier2 = new JButton(imagerd);	
+		redsoldier2.setBounds(184, 399, 52, 52);
+		redsoldier2.setContentAreaFilled(false);
+		redsoldier2.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(redsoldier2);
+		redsoldier2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 2;
+			}
+		});
+		//ºì·½±ø3
+		redsoldier3 = new JButton(imagerd);	
+		redsoldier3.setBounds(299, 399, 52, 52);
+		redsoldier3.setContentAreaFilled(false);
+		redsoldier3.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(redsoldier3);
+		redsoldier3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 3;
+			}
+		});
+		//ºì·½±ø4
+		redsoldier4 = new JButton(imagerd);	
+		redsoldier4.setBounds(415, 399, 52, 52);
+		redsoldier4.setContentAreaFilled(false);
+		redsoldier4.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(redsoldier4);
+		redsoldier4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 4;
+			}
+		});
+		//ºì·½±ø5
+		redsoldier5 = new JButton(imagerd);	
+		redsoldier5.setBounds(530, 399, 52, 52);
+		redsoldier5.setContentAreaFilled(false);
+		redsoldier5.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(redsoldier5);
+		redsoldier5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});
+		//ºì·½ÅÚ×ó
+		ImageIcon imagegun;
+		imagegun = new ImageIcon("images/redgun.png");
+		Lredgun = new JButton(imagegun);	
+		Lredgun.setBounds(127, 449, 52, 52);
+		Lredgun.setContentAreaFilled(false);
+		Lredgun.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lredgun);
+		Lredgun.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºì·½ÅÚÓÒ
+		Rredgun = new JButton(imagegun);	
+		Rredgun.setBounds(473, 449, 52, 52);
+		Rredgun.setContentAreaFilled(false);
+		Rredgun.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rredgun);
+		Rredgun.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºì³µ×ó
+		ImageIcon imagecar;
+		imagecar = new ImageIcon("images/redcar.png");
+		Lredcar = new JButton(imagecar);	
+		Lredcar.setBounds(71, 550, 52, 52);
+		Lredcar.setContentAreaFilled(false);
+		Lredcar.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lredcar);
+		Lredcar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºì³µÓÒ
+		Rredcar = new JButton(imagecar);	
+		Rredcar.setBounds(530, 550, 52, 52);
+		Rredcar.setContentAreaFilled(false);
+		Rredcar.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rredcar);
+		Rredcar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºìÂí×ó
+		ImageIcon imagehorse;
+		imagehorse = new ImageIcon("images/redhorse.png");
+		Lredhorse = new JButton(imagehorse);	
+		Lredhorse.setBounds(127, 550, 52, 52);
+		Lredhorse.setContentAreaFilled(false);
+		Lredhorse.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lredhorse);
+		Lredhorse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºìÂíÓÒ
+		Rredhorse = new JButton(imagehorse);	
+		Rredhorse.setBounds(473, 550, 52, 52);
+		Rredhorse.setContentAreaFilled(false);
+		Rredhorse.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rredhorse);
+		Rredhorse.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºìÏó×ó
+		ImageIcon imageelephant;
+		imageelephant = new ImageIcon("images/redelephant.png");
+		Lredelephant = new JButton(imageelephant);	
+		Lredelephant.setBounds(184, 550, 52, 52);
+		Lredelephant.setContentAreaFilled(false);
+		Lredelephant.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lredelephant);
+		Lredelephant.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºìÏóÓÒ
+		Rredelephant = new JButton(imageelephant);	
+		Rredelephant.setBounds(416, 550, 52, 52);
+		Rredelephant.setContentAreaFilled(false);
+		Rredelephant.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rredelephant);
+		Rredelephant.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºìÊ¿×ó
+		ImageIcon imageknight;
+		imageknight = new ImageIcon("images/redknight.png");
+		Lredknight = new JButton(imageknight);	
+		Lredknight.setBounds(241, 550, 52, 52);
+		Lredknight.setContentAreaFilled(false);
+		Lredknight.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Lredknight);
+		Lredknight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});		
+		//ºìÊ¿ÓÒ
+		Rredknight = new JButton(imageknight);	
+		Rredknight.setBounds(359, 550, 52, 52);
+		Rredknight.setContentAreaFilled(false);
+		Rredknight.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(Rredknight);
+		Rredknight.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});	
+		//ºìË§
+		ImageIcon imagecommander;
+		imagecommander = new ImageIcon("images/redcommander.png");
+		redcommander = new JButton(imagecommander);	
+		redcommander.setBounds(298, 550, 52, 52);
+		redcommander.setContentAreaFilled(false);
+		redcommander.setBorderPainted(false);
+		frame.getContentPane().setLayout(null); 
+		frame.getContentPane().add(redcommander);
+		redcommander.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				rdClicked = true;
+				rd = 5;
+			}
+		});			
 		frame.addMouseListener(new MyFrameMouse());
-
 		frame.setVisible(true); 
-		frame.setResizable(false);
+		frame.setResizable(false);		
+		
 	}
 	public static void main(String[] args) {
 		chessboard();
--- ChineseChess/src/gamestart/MyFrameMouse.java
+++ ChineseChess/src/gamestart/MyFrameMouse.java
@@ -9,17 +9,47 @@ public class MyFrameMouse implements MouseListener {
 
 	 public void mouseClicked(MouseEvent e) {
 		 int x,y;
-         if(Gamestart.isClicked){
-             Point dp = e.getPoint();//得到点击的位置
-             x=dp.x;
-             y=dp.y-33;
-             System.out.println(y);
-             if(x > 72 && x < 123 && y > 343 && y < 397) {
-            	 System.out.println("***********");
-            	 Gamestart.soldierbutton1.setLocation(71,343);
-            	 Gamestart.isClicked=false;
-             }
-         }
+		 if(Gamestart.rdClicked) {
+	         Point dp = e.getPoint();//得到点击的位置
+	         x=dp.x;
+	         y=dp.y-33;
+//	         System.out.println(y);
+	         if(Gamestart.rd == 1) {	//判断棋子
+		         if(x > 72 && x < 123 && y > 343 && y < 397) {		//设置范围
+			         Gamestart.redsoldier1.setLocation(71,343);		//设置位置
+			         Gamestart.rdClicked = false;
+			         Gamestart.rd = 0;
+		         }
+	         }
+	         if(Gamestart.rd == 2) {
+		         if(x > 184 && x < 235 && y > 343 && y < 397) {
+			         Gamestart.redsoldier2.setLocation(184,343);
+			         Gamestart.rdClicked = false;
+			         Gamestart.rd = 0;
+		         }
+	         }
+	         if(Gamestart.rd == 3) {
+		         if(x > 299 && x < 351 && y > 343 && y < 397) {
+			         Gamestart.redsoldier3.setLocation(299,343);
+			         Gamestart.rdClicked = false;
+			         Gamestart.rd = 0;
+		         }
+	         }
+	         if(Gamestart.rd == 4) {
+		         if(x > 415 && x < 467 && y > 343 && y < 397) {
+			         Gamestart.redsoldier4.setLocation(415,343);
+			         Gamestart.rdClicked = false;
+			         Gamestart.rd = 0;
+		         }
+	         }
+	         if(Gamestart.rd == 5) {
+		         if(x > 530 && x < 582 && y > 343 && y < 397) {
+			         Gamestart.redsoldier5.setLocation(530,343);
+			         Gamestart.rdClicked = false;
+			         Gamestart.rd = 0;
+		         }
+	         }
+		 }
      }
 	@Override
 	public void mouseEntered(MouseEvent e) {
