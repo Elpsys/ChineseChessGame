c98a51558cbf91bf9725a2bc8970816e64f21246
Elpsys <1437432373@qq.com>
2019-06-28 15:13:56 +0800

    Update MyFrameMouse.java

--- ChineseChess/src/gamestart/MyFrameMouse.java
+++ ChineseChess/src/gamestart/MyFrameMouse.java
@@ -55,7 +55,7 @@ public class MyFrameMouse implements MouseListener {
 			        				 }
 			        				//判断左走的位置
 			        				 if(Gamestart.redsoldier1.getX() != clx[0]) {
-				        				 if(x > clx[i-1] && x < clx[i-2] && y > cly[j] && y < cly[j+1]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
 				        					 Gamestart.redsoldier1.setLocation(clx[i-1],cly[j]);		
 									         Gamestart.rdClicked = false;
 									         Gamestart.rmove = false;
@@ -101,7 +101,7 @@ public class MyFrameMouse implements MouseListener {
 			        				 }
 			        				//判断左走的位置
 			        				 if(Gamestart.redsoldier2.getX() != clx[0]) {
-				        				 if(x > clx[i-1] && x < clx[i-2] && y > cly[j] && y < cly[j+1]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
 				        					 Gamestart.redsoldier2.setLocation(clx[i-1],cly[j]);		
 									         Gamestart.rdClicked = false;
 									         Gamestart.rmove = false;
@@ -146,7 +146,7 @@ public class MyFrameMouse implements MouseListener {
 			        				 }
 			        				//判断左走的位置
 			        				 if(Gamestart.redsoldier3.getX() != clx[0]) {
-				        				 if(x > clx[i-1] && x < clx[i-2] && y > cly[j] && y < cly[j+1]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
 				        					 Gamestart.redsoldier3.setLocation(clx[i-1],cly[j]);		
 									         Gamestart.rdClicked = false;
 									         Gamestart.rmove = false;
@@ -191,7 +191,7 @@ public class MyFrameMouse implements MouseListener {
 			        				 }
 			        				//判断左走的位置
 			        				 if(Gamestart.redsoldier4.getX() != clx[0]) {
-				        				 if(x > clx[i-1] && x < clx[i-2] && y > cly[j] && y < cly[j+1]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
 				        					 Gamestart.redsoldier4.setLocation(clx[i-1],cly[j]);		
 									         Gamestart.rdClicked = false;
 									         Gamestart.rmove = false;
@@ -236,7 +236,7 @@ public class MyFrameMouse implements MouseListener {
 			        				 }
 			        				//判断左走的位置
 			        				 if(Gamestart.redsoldier5.getX() != clx[0]) {
-				        				 if(x > clx[i-1] && x < clx[i-2] && y > cly[j] && y < cly[j+1]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
 				        					 Gamestart.redsoldier5.setLocation(clx[i-1],cly[j]);		
 									         Gamestart.rdClicked = false;
 									         Gamestart.rmove = false;
@@ -252,63 +252,6 @@ public class MyFrameMouse implements MouseListener {
 				 ErrorTip.tip();
 			 }
          }
-         //红色兵
-     /*    if(Gamestart.rdClicked) {
-			 if(Gamestart.rmove) {
-		        if(Gamestart.rd == 1) {	//判断棋子
-			         if(x > clx[0] && x < clx[1] && y > cly[5] && y < cly[6]) {//设置范围
-			        	 if(Gamestart.redsoldier1.getX() == clx[0] && Gamestart.redsoldier1.getY() == cly[6]) {
-					         Gamestart.redsoldier1.setLocation(clx[0],cly[5]);		//设置位置
-					         Gamestart.rdClicked = false;
-					         Gamestart.rmove = false;
-							 Gamestart.bmove = true;
-			        	 }
-			         }
-		         }
-		         if(Gamestart.rd == 2) {
-			         if(x > clx[2] && x < clx[3] && y > cly[5] && y < cly[6]) {
-			        	 if(Gamestart.redsoldier2.getX() == clx[2] && Gamestart.redsoldier2.getY() == cly[6]) {
-					         Gamestart.redsoldier2.setLocation(clx[2],cly[5]);
-					         Gamestart.rdClicked = false;
-					         Gamestart.rmove = false;
-							 Gamestart.bmove = true;
-			        	 }
-			         }
-		         }
-		         if(Gamestart.rd == 3) {
-			         if(x > clx[4] && x < clx[5] && y > cly[5] && y < cly[6]) {
-			        	 if(Gamestart.redsoldier3.getX() == clx[4] && Gamestart.redsoldier3.getY() == cly[6]) {
-					         Gamestart.redsoldier3.setLocation(clx[4],cly[5]);
-					         Gamestart.rdClicked = false;
-					         Gamestart.rmove = false;
-							 Gamestart.bmove = true;
-			        	 }
-			         }
-		         }
-		         if(Gamestart.rd == 4) {
-			         if(x > clx[6] && x < clx[7] && y > cly[5] && y < cly[6]) {
-			        	 if(Gamestart.redsoldier4.getX() == clx[6] && Gamestart.redsoldier4.getY() == cly[6]) {
-					         Gamestart.redsoldier4.setLocation(clx[6],cly[5]);
-					         Gamestart.rdClicked = false;
-					         Gamestart.rmove = false;
-							 Gamestart.bmove = true;
-			        	 }
-			         }
-		         }
-		         if(Gamestart.rd == 5) {
-			         if(x > clx[8] && x < clx[9] && y > cly[5] && y < cly[6]) {
-			        	 if(Gamestart.redsoldier5.getX() == clx[8] && Gamestart.redsoldier5.getY() == cly[6]) {
-					         Gamestart.redsoldier5.setLocation(clx[8],cly[5]);
-					         Gamestart.rdClicked = false;
-					         Gamestart.rmove = false;
-							 Gamestart.bmove = true;
-			        	 }
-			         }
-		         }
-			 }else {
-				 ErrorTip.tip();
-			 }
-         }*/
          //红炮
          if(Gamestart.rgClicked) {
 			 if(Gamestart.rmove) {
@@ -323,59 +266,245 @@ public class MyFrameMouse implements MouseListener {
 				 ErrorTip.tip();
 			 }
          }
-		 //黑色棋子
+		 //--------------------------黑色棋子----------------------------------
+         //黑兵--------------
          if(Gamestart.bdClicked) {
 			 if(Gamestart.bmove) {
-		         if(Gamestart.bd == 1) {	//判断棋子
-			         if(x > clx[0] && x < clx[1] && y > cly[4] && y < cly[5]) {		//设置范围
-			        	 if(Gamestart.blacksoldier1.getX() == clx[0] && Gamestart.blacksoldier1.getY() == cly[3]) {
-					         Gamestart.blacksoldier1.setLocation(clx[0],cly[4]);		//设置位置
-					         Gamestart.bdClicked = false;
-							 Gamestart.bmove = false;
-							 Gamestart.rmove = true;
+				 if(Gamestart.bd == 1) {
+			         for(i = 0;i <= 8;i ++) {
+			        	 for(j = 0;j <=9;j ++) {
+			        		 if(Gamestart.blacksoldier1.getX() == clx[i] && Gamestart.blacksoldier1.getY() == cly[j]) {
+			        			 if(cly[j] < 347) {
+			        				 //过河之前只能向前走
+			        				 System.out.println("---------------------------------");
+			        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+				        				 Gamestart.blacksoldier1.setLocation(clx[i],cly[j+1]);		//设置位置
+								         Gamestart.bdClicked = false;
+								         Gamestart.bmove = false;
+										 Gamestart.rmove = true;
+			        				 }
+			        			 }else {
+			        				 //判断向前走的位置
+			        				 if(Gamestart.blacksoldier1.getY() != cly[0]) {
+				        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+					        				 Gamestart.blacksoldier1.setLocation(clx[i],cly[j+1]);		//设置位置
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				 //判断右走的位置
+			        				 if(Gamestart.blacksoldier1.getX() != clx[8]) {
+				        				 if(x > clx[i+1] && x < clx[i+2] && y > cly[j] && y < cly[j+1]) {
+					        				 Gamestart.blacksoldier1.setLocation(clx[i+1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				//判断左走的位置
+			        				 if(Gamestart.blacksoldier1.getX() != clx[0]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
+				        					 Gamestart.blacksoldier1.setLocation(clx[i-1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        			 }
+			        		 }
+			        		 
 			        	 }
 			         }
-		         }
-		         if(Gamestart.bd == 2) {
-			         if(x > clx[2] && x < clx[3] && y > cly[4] && y < cly[5]) {
-			        	 if(Gamestart.blacksoldier2.getX() == clx[2] && Gamestart.blacksoldier2.getY() == cly[3]) {
-					         Gamestart.blacksoldier2.setLocation(clx[2],cly[4]);
-					         Gamestart.bdClicked = false;
-							 Gamestart.bmove = false;
-							 Gamestart.rmove = true;
+				 }
+				 if(Gamestart.bd == 2) {
+			         for(i = 0;i <= 8;i ++) {
+			        	 for(j = 0;j <=9;j ++) {
+			        		 if(Gamestart.blacksoldier2.getX() == clx[i] && Gamestart.blacksoldier2.getY() == cly[j]) {
+			        			 if(cly[j] < 347) {
+			        				 //过河之前只能向前走
+			        				 System.out.println("---------------------------------");
+			        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+				        				 Gamestart.blacksoldier2.setLocation(clx[i],cly[j+1]);		//设置位置
+								         Gamestart.bdClicked = false;
+								         Gamestart.bmove = false;
+										 Gamestart.rmove = true;
+			        				 }
+			        			 }else {
+			        				 //判断向前走的位置
+			        				 if(Gamestart.blacksoldier2.getY() != cly[0]) {
+				        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+					        				 Gamestart.blacksoldier1.setLocation(clx[i],cly[j+1]);		//设置位置
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				 //判断右走的位置
+			        				 if(Gamestart.blacksoldier2.getX() != clx[8]) {
+				        				 if(x > clx[i+1] && x < clx[i+2] && y > cly[j] && y < cly[j+1]) {
+					        				 Gamestart.blacksoldier2.setLocation(clx[i+1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				//判断左走的位置
+			        				 if(Gamestart.blacksoldier2.getX() != clx[0]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
+				        					 Gamestart.blacksoldier2.setLocation(clx[i-1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        			 }
+			        		 }
+			        		 
 			        	 }
 			         }
-		         }
-		         if(Gamestart.bd == 3) {
-			         if(x > clx[4] && x < clx[5] && y > cly[4] && y < cly[5]) {
-			        	 if(Gamestart.blacksoldier3.getX() == clx[4] && Gamestart.blacksoldier3.getY() == cly[3]) {
-					         Gamestart.blacksoldier3.setLocation(clx[4],cly[4]);
-					         Gamestart.bdClicked = false;
-							 Gamestart.bmove = false;
-							 Gamestart.rmove = true;
+				 }
+				 if(Gamestart.bd == 3) {
+			         for(i = 0;i <= 8;i ++) {
+			        	 for(j = 0;j <=9;j ++) {
+			        		 if(Gamestart.blacksoldier3.getX() == clx[i] && Gamestart.blacksoldier3.getY() == cly[j]) {
+			        			 if(cly[j] < 347) {
+			        				 //过河之前只能向前走
+			        				 System.out.println("---------------------------------");
+			        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+				        				 Gamestart.blacksoldier3.setLocation(clx[i],cly[j+1]);		//设置位置
+								         Gamestart.bdClicked = false;
+								         Gamestart.bmove = false;
+										 Gamestart.rmove = true;
+			        				 }
+			        			 }else {
+			        				 //判断向前走的位置
+			        				 if(Gamestart.blacksoldier3.getY() != cly[0]) {
+				        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+					        				 Gamestart.blacksoldier3.setLocation(clx[i],cly[j+1]);		//设置位置
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				 //判断右走的位置
+			        				 if(Gamestart.blacksoldier3.getX() != clx[8]) {
+				        				 if(x > clx[i+1] && x < clx[i+2] && y > cly[j] && y < cly[j+1]) {
+					        				 Gamestart.blacksoldier3.setLocation(clx[i+1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				//判断左走的位置
+			        				 if(Gamestart.blacksoldier3.getX() != clx[0]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
+				        					 Gamestart.blacksoldier3.setLocation(clx[i-1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        			 }
+			        		 }
+			        		 
 			        	 }
 			         }
-		         }
-		         if(Gamestart.bd == 4) {
-			         if(x > clx[6] && x < clx[7] && y > cly[4] && y < cly[5]) {
-			        	 if(Gamestart.blacksoldier4.getX() == clx[6] && Gamestart.blacksoldier4.getY() == cly[3]) {
-					         Gamestart.blacksoldier4.setLocation(clx[6],cly[4]);
-					         Gamestart.bdClicked = false;
-							 Gamestart.bmove = false;
-							 Gamestart.rmove = true;
+				 }
+				 if(Gamestart.bd == 4) {
+			         for(i = 0;i <= 8;i ++) {
+			        	 for(j = 0;j <=9;j ++) {
+			        		 if(Gamestart.blacksoldier4.getX() == clx[i] && Gamestart.blacksoldier4.getY() == cly[j]) {
+			        			 if(cly[j] < 347) {
+			        				 //过河之前只能向前走
+			        				 System.out.println("---------------------------------");
+			        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+				        				 Gamestart.blacksoldier4.setLocation(clx[i],cly[j+1]);		//设置位置
+								         Gamestart.bdClicked = false;
+								         Gamestart.bmove = false;
+										 Gamestart.rmove = true;
+			        				 }
+			        			 }else {
+			        				 //判断向前走的位置
+			        				 if(Gamestart.blacksoldier4.getY() != cly[0]) {
+				        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+					        				 Gamestart.blacksoldier4.setLocation(clx[i],cly[j+1]);		//设置位置
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				 //判断右走的位置
+			        				 if(Gamestart.blacksoldier4.getX() != clx[8]) {
+				        				 if(x > clx[i+1] && x < clx[i+2] && y > cly[j] && y < cly[j+1]) {
+					        				 Gamestart.blacksoldier4.setLocation(clx[i+1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				//判断左走的位置
+			        				 if(Gamestart.blacksoldier4.getX() != clx[0]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
+				        					 Gamestart.blacksoldier4.setLocation(clx[i-1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        			 }
+			        		 }
+			        		 
 			        	 }
 			         }
-		         }
-		         if(Gamestart.bd == 5) {
-			         if(x > clx[8] && x < clx[8]+53 && y > cly[4] && y < cly[5]) {
-			        	 if(Gamestart.blacksoldier5.getX() == clx[8] && Gamestart.blacksoldier5.getY() == cly[3]) {
-					         Gamestart.blacksoldier5.setLocation(clx[8],cly[4]);
-					         Gamestart.bdClicked = false;
-							 Gamestart.bmove = false;
-							 Gamestart.rmove = true;
+				 }
+				 if(Gamestart.bd == 5) {
+			         for(i = 0;i <= 8;i ++) {
+			        	 for(j = 0;j <=9;j ++) {
+			        		 if(Gamestart.blacksoldier5.getX() == clx[i] && Gamestart.blacksoldier5.getY() == cly[j]) {
+			        			 if(cly[j] < 347) {
+			        				 //过河之前只能向前走
+			        				 System.out.println("---------------------------------");
+			        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+				        				 Gamestart.blacksoldier5.setLocation(clx[i],cly[j+1]);		//设置位置
+								         Gamestart.bdClicked = false;
+								         Gamestart.bmove = false;
+										 Gamestart.rmove = true;
+			        				 }
+			        			 }else {
+			        				 //判断向前走的位置
+			        				 if(Gamestart.blacksoldier5.getY() != cly[0]) {
+				        				 if(x > clx[i] && x < clx[i+1] && y > cly[j+1] && y < cly[j+2]) {
+					        				 Gamestart.blacksoldier5.setLocation(clx[i],cly[j+1]);		//设置位置
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				 //判断右走的位置
+			        				 if(Gamestart.blacksoldier5.getX() != clx[8]) {
+				        				 if(x > clx[i+1] && x < clx[i+2] && y > cly[j] && y < cly[j+1]) {
+					        				 Gamestart.blacksoldier5.setLocation(clx[i+1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        				//判断左走的位置
+			        				 if(Gamestart.blacksoldier5.getX() != clx[0]) {
+				        				 if(x > clx[i-1] && x < clx[i] && y > cly[j] && y < cly[j+1]) {
+				        					 Gamestart.blacksoldier5.setLocation(clx[i-1],cly[j]);		
+									         Gamestart.bdClicked = false;
+									         Gamestart.bmove = false;
+											 Gamestart.rmove = true;
+				        				 }
+			        				 }
+			        			 }
+			        		 }
+			        		 
 			        	 }
 			         }
-		         }
+				 }
 			 }else {
 				 ErrorTip.tip();
 			 }
